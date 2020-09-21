b117c32787a92d6a5d2f671496df69cfe924e129
Karthick Ragavendran <karthickragavendran@Karthicks-MacBook-Air.local>
2020-09-21 14:40:56 +0530

    Made the db calls async

--- src/util/index.js
+++ src/util/index.js
@@ -1,3 +1,7 @@
 export const centsToDollar = (cents) => {
   return `$${(cents / 100).toFixed(2)}`
 }
+
+export const wait = (ms = Math.random() * 1000) => {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
